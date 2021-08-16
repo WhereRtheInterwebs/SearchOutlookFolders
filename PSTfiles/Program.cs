af5234a0ed97308614d62b9fc2c9a7e5914f5070
WhereRtheInterwebs <16965015+WhereRtheInterwebs@users.noreply.github.com>
2021-08-16 16:38:15 -0500

    Add project files.

---
+++ PSTfiles/Program.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Office.Interop.Outlook;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace PSTfiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string LogFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "PSTfiles.txt");
+
+            Application app = new Application();
+            NameSpace ns = app.GetNamespace("MAPI");
+            MAPIFolder rootFolder = ns.DefaultStore.GetRootFolder();
+
+            List<Folder> allFolders = GetFolders(rootFolder.Folders).ToList();
+
+            File.WriteAllLines(LogFilePath, allFolders.Select(x => x.FullFolderPath).ToArray());
+
+            string searchResult = "";
+            if (args.Any())
+            {
+                searchResult = "Found: " + (allFolders.FirstOrDefault(x => x.Name.ToLower().Contains(args[0].ToLower()))?.FullFolderPath ?? "Nothing :(");
+                Console.WriteLine(searchResult);
+                File.AppendAllLines(LogFilePath, new[] { searchResult });
+            }
+
+            if (Debugger.IsAttached | true)
+            {
+                Console.WriteLine("Press any key to exit.");
+                Console.ReadKey();
+            }
+        }
+
+        static IEnumerable<Folder> GetFolders(Folders folders)
+        {
+            foreach (Folder folder in folders)
+            {
+                yield return folder;
+                Console.WriteLine(folder.FullFolderPath);
+                if (folder.Folders != null)
+                    foreach (Folder subFolder in GetFolders(folder.Folders))
+                        yield return subFolder;
+            }
+        }
+    }
+}
---
+++ SearchOutlookFolders/SearchFolders.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Management.Automation;
+using Microsoft.Office.Interop.Outlook;
+
+namespace SearchOutlookFolders
+{
+    [Cmdlet(VerbsCommon.Search, "OutlookFolders")]
+    [OutputType(typeof(SearchResult))]
+    public class SearchFolders : Cmdlet
+    {
+        [Parameter(Position = 1)]
+        public string SearchText { get; set; } = "";
+
+        [Parameter(Position = 2)]
+        public string Store { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            Application app = new Application();
+            NameSpace ns = app.GetNamespace("MAPI");
+            MAPIFolder rootFolder = ns.DefaultStore.GetRootFolder();
+
+            if (Store != null)
+                rootFolder = ns.Stores[Store].GetRootFolder();
+
+            List<Folder> allFolders = GetFolders(rootFolder.Folders).ToList();
+
+            allFolders.Where(x => x.Name.ToLower().Contains(SearchText.ToLower()))
+                      .Select(x => new SearchResult
+                      {
+                          Name     = x.Name,
+                          FullPath = x.FullFolderPath
+                      })
+                      .ToList()
+                      .ForEach(WriteObject);
+        }
+
+        IEnumerable<Folder> GetFolders(Folders folders)
+        {
+            foreach (Folder folder in folders)
+            {
+                yield return folder;
+                WriteVerbose(folder.FullFolderPath);
+                if (folder.Folders != null)
+                    foreach (Folder subFolder in GetFolders(folder.Folders))
+                        yield return subFolder;
+            }
+        }
+
+        [Cmdlet(VerbsCommon.Get, "OutlookStores")]
+        [OutputType(typeof(string))]
+        public class GetStores : Cmdlet
+        {
+            protected override void ProcessRecord()
+            {
+                Application app = new Application();
+                NameSpace ns = app.GetNamespace("MAPI");
+                ns.Stores.Cast<Store>().Select(x => x.DisplayName).ToList().ForEach(WriteObject);
+            }
+        }
+    }
+
+    public class SearchResult
+    {
+        public string Name { get; set; }
+        public string FullPath { get; set; }
+    }
+}
